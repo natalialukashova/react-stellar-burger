9eaa5a02ae13b95d0543b2a8d3efb763a5b2869c
Natalia Lukashova <naluva.work@gmail.com>
2023-05-28 14:56:14 +0300

    текущее состояние

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -49,5 +49,5 @@ export default function Modal({ closeModal, children, headerModal = "" }) {
 Modal.propTypes = {
   children: PropTypes.element,
   closeModal: PropTypes.func,
-  headerModal: PropTypes.string,
+  headerModal: PropTypes.object,
 };
