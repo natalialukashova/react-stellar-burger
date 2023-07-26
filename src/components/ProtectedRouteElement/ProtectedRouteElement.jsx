9b134e53d02661b60aef925a30f38185fa54c064
Natalia Lukashova <naluva.work@gmail.com>
2023-07-26 22:02:06 +0300

    исправление ошибки в профиле

--- src/components/ProtectedRouteElement/ProtectedRouteElement.jsx
+++ src/components/ProtectedRouteElement/ProtectedRouteElement.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../../utils/auth";
 import PropTypes from "prop-types";
+import { api } from "../../Api/Api";
 
 export default function ProtectedRouteElement({element}) {
-  let { verificationUser, ...auth } = useAuth();
   const [isUserLoaded, setUserLoaded] = useState(false);
 
   const init = async () => {
-    await verificationUser();
+    await api.getUserRequest();
     setUserLoaded(true);
   }
 
